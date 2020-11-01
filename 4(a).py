32043a0268d7fb98f71bfe0ae9117f27e2cc0eaa
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2020-11-02 00:12:38 +0530

    4th done

--- 4(a).py
+++ 4(a).py
@@ -9,6 +9,7 @@ def insert_entry(arr, name, number):
 
     for name_index in range(len(arr)):
         if name > arr[-1]:
+            print(f'inserting {name} at end')
             arr.append(name)
             phonebookNumbers.append(number)
             break
@@ -16,7 +17,7 @@ def insert_entry(arr, name, number):
             continue
 
         else:
-            print(f'inserting at {name} at {name_index}')
+            print(f'inserting {name} at {name_index}')
             arr.insert(name_index, name)
             phonebookNumbers.insert(name_index, number)
             break
@@ -26,52 +27,40 @@ def bin_rec_search(name, arr, start, end):
     print(start, end)
     mid = int((end-start)//2+start)
 
-    # Not found block
-    if end-start <= 1:
-        if name == arr[start]:
-            return start
-        elif name == arr[end]:
-            return end
-        else:
-            print("___NOT FOUND___")
-            number = input(f"plz enter {name}'s phone number : ")
-            insert_entry(arr, name, number)
-            return -1
+    if end < start:
+        print("___NOT FOUND___")
+        number = input(f"plz enter {name}'s phone number : ")
+        insert_entry(arr, name, number)
+        return -1
 
     if name == arr[mid]:
         print('Exactly mid')
         return mid
     elif name > arr[mid]:
         print('greater than mid')
-        return bin_rec_search(name, arr, mid, end)
+        return bin_rec_search(name, arr, mid+1, end)
     elif name < arr[mid]:
         print('smaller than mid')
-        return bin_rec_search(name, arr, start, mid)
+        return bin_rec_search(name, arr, start, mid-1)
 
 
 def bin_ite_search(name, arr, start, end):
     # Not found block
     while start <= end:
 
-        if end - start <= 1:
-            if name == arr[start]:
-                return start
-            elif name == arr[end]:
-                return end
-            else:
-                print("___NOT FOUND___")
-                number = input(f"plz enter {name}'s phone number : ")
-                insert_entry(arr, name, number)
-                return -1
-
         # print(start, end)
         mid = int((end-start)//2+start)
         if name == arr[mid]:
             return mid
         elif name < arr[mid]:
-            end = mid
+            end = mid - 1
         elif name > arr[mid]:
-            start = mid
+            start = mid + 1
+    else:
+        print("___NOT FOUND___")
+        number = input(f"plz enter {name}'s phone number : ")
+        insert_entry(arr, name, number)
+        return -1
 
 
 #                   0           1       2       3       4       5           6       7       8       9
